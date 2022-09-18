e20b44d25cdf2e288e206555d2312edf0f22653c
amcilraith10 <amcilraith@smith.edu>
2022-09-18 19:37:03 -0400

    Second commit! Hopefully the last one!

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -5,15 +5,17 @@ class ResaleShop:
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    inventory : Dict[int, Dict[str, Union[str, int, bool]]] = {}
-
+   
+# Added attributes here (why is the self attribute a different color?)
     def __init__(self, inventory):
+        #Nested dictionary: first dictionary is an integer 
+        # which is mapped onto another dictionary with a string which is mapped onto a union?? :/
+        inventory : Dict[int, Dict[str, Union[str, int, bool]]] = {}
         def buy(self, computer: Dict[str, Union[str, int, bool]]):
             global itemID
             itemID += 1 # increment itemID
             self.inventory[itemID] = computer
             return itemID
-        
         def print_inventory(self):
             # If the inventory is not empty
             if inventory:
@@ -30,8 +32,12 @@ class ResaleShop:
                 print("Item", item_id, "sold!")
             else: 
                 print("Item", item_id, "not found. Please select another item to sell.")
+                
+        sell(1)
+
         
 
 
         
     # What methods will you need?
+
